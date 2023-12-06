ea65230549bdcca31cc54f8cbefe57d6273ce74e
Emilio Gajardo <ej.gajardo@gmail.com>
2023-12-06 15:59:51 -0300

    Configuracion de cubit en un contador

--- lib/presentation/screens/cubit_counter_screen.dart
+++ lib/presentation/screens/cubit_counter_screen.dart
@@ -1,42 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:forms_app/presentation/blocs/counter_cubit/counter_cubit.dart';
 
 class CubitCounterScreen extends StatelessWidget {
   const CubitCounterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => CounterCubit(),
+      child: const _CubitCounterView(),
+    );
+  }
+}
+
+class _CubitCounterView extends StatelessWidget {
+  const _CubitCounterView();
+
+  @override
+  Widget build(BuildContext context) {
+
+    // final counterState = context.watch<CounterCubit>().state;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cubit Counter'),
+        title: context.select((CounterCubit counterCubit) {
+          return Text('Cubit Counter Transactions: ${counterCubit.state.transactionCount}');// muestra la cantidad de transacciones
+        },),
+
         actions: [
-          IconButton(onPressed: () => {}, icon: const Icon(Icons.refresh_outlined))
+          IconButton(
+            onPressed: () => context.read<CounterCubit>().reset(),
+            icon: const Icon(Icons.refresh_outlined)
+          )
         ],
       ),
-      body: const Center(
-        child: Text('Counter value: x'),
+      body: Center(
+        child: BlocBuilder<CounterCubit, CounterState>(
+          // buildWhen: (previous, current) => current.counter != previous.counter,// cambia el estado solo si cambia el counter
+          builder: (context, state) {
+            print('>> Counter cambio');
+            return Text('Counter value \n${state.counter}', style: const TextStyle(fontSize: 40), textAlign: TextAlign.center,);
+          },
+        ),
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          FloatingActionButton(
-            heroTag: '1',
-            child: const Text('+1'),
-            onPressed: () => {}
-          ),
-          const SizedBox(height: 20,),
-          FloatingActionButton(
-            heroTag: '2',
-            child: const Text('+2'),
-            onPressed: () => {}
-          ),
-          const SizedBox(height: 20,),
-          FloatingActionButton(
-            heroTag: '3',
-            child: const Text('+3'),
-            onPressed: () => {}
-          ),
+          increaseByFloatingActionButtons(1, context),
+          const SizedBox(height: 20),
+          increaseByFloatingActionButtons(2, context),
+          const SizedBox(height: 20),
+          increaseByFloatingActionButtons(3, context),
         ],
       ),
+    );
+  }
+
+  Widget increaseByFloatingActionButtons(int cantidad, BuildContext context) {
+    return FloatingActionButton(
+      heroTag: '$cantidad',
+      child: Text('+$cantidad'),
+      onPressed: () => context.read<CounterCubit>().increaseBy(cantidad)
     );
   }
 }
